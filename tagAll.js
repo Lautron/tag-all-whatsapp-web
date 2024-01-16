c8c3cbe87f145d3c7027c474679d4b4d926f0e9d
Lautaro Bachmann <laurigbach@gmail.com>
2024-01-16 14:07:14 -0300

    Update script to accomodate some changes on the website

--- tagAll.js
+++ tagAll.js
@@ -6,11 +6,13 @@
 
 // Constants declared with 'let' to prevent errors if executing script several times
 let MAX_CONSECUTIVE_SAME_SIZES = 3;
+// selector to select all names in the list of members. Inspect the span elem that contains the name of the member to find it.
+// You'll also have to add a parent elem selector to avoid selecting your own chats
 let MEMBER_LIST_SELECTOR =
-  "#app > div > span:nth-child(2) > div > span > div > div > div > div > div > div > div._3Bc7H.g0rxnol2.thghmljt.p357zi0d.rjo8vgbg.ggj6brxn.f8m0rgwh.gfz4du6o.ag5g9lrv.bs7a17vp.ov67bkzj > div > div > div > div div div div._3OvU8 div._3vPI2 div span";
+    "div.g0rxnol2.g0rxnol2.thghmljt.p357zi0d.rjo8vgbg.ggj6brxn.f8m0rgwh.gfz4du6o.ag5g9lrv.bs7a17vp.ov67bkzj div.y_sn4 > div > div > span";
 let SCROLL_PIXELS = 500;
 let SCROLL_ELEM_CLASSNAME =
-  "_3Bc7H g0rxnol2 thghmljt p357zi0d rjo8vgbg ggj6brxn f8m0rgwh gfz4du6o ag5g9lrv bs7a17vp ov67bkzj";
+    "g0rxnol2 g0rxnol2 thghmljt p357zi0d rjo8vgbg ggj6brxn f8m0rgwh gfz4du6o ag5g9lrv bs7a17vp ov67bkzj";
 
 let members = new Set();
 let prevMemberSize = 0;
@@ -18,52 +20,55 @@ let consecutiveSameSizes = 0;
 let scrapingIntervalID = null;
 
 let getMemberElems = () => {
-  return document.querySelectorAll(MEMBER_LIST_SELECTOR);
+    return document.querySelectorAll(MEMBER_LIST_SELECTOR);
 };
 
 let addElemsTextToMembers = (membersList) =>
-  membersList.forEach((elem) => members.add(elem.innerText));
+    membersList.forEach((elem) => {
+        members.add(elem.innerText)
+    }
+    );
 
 let scrollDown = () => {
-  document
-    .getElementsByClassName(SCROLL_ELEM_CLASSNAME)[0]
-    .scrollBy(0, SCROLL_PIXELS);
+    document
+        .getElementsByClassName(SCROLL_ELEM_CLASSNAME)[0]
+        .scrollBy(0, SCROLL_PIXELS);
 };
 
 let updateConsecutiveSameSizes = (memberSize) => {
-  if (prevMemberSize === memberSize) {
-    consecutiveSameSizes++;
-  } else {
-    consecutiveSameSizes = 0;
-  }
-  prevMemberSize = memberSize;
+    if (prevMemberSize === memberSize) {
+        consecutiveSameSizes++;
+    } else {
+        consecutiveSameSizes = 0;
+    }
+    prevMemberSize = memberSize;
 };
 
 let stopScraping = () => {
-  console.log("Stopping scraper...");
-  clearInterval(scrapingIntervalID);
+    console.log("Stopping scraper...");
+    clearInterval(scrapingIntervalID);
 };
 
 let giveOutput = () => {
-  let membersArray = Array.from(members);
-  let text = "@" + membersArray.join("\n@");
-  console.log(text);
-  console.log("Size: ", members.size);
+    let membersArray = Array.from(members);
+    let text = "@" + membersArray.join("\n@");
+    console.log(text);
+    console.log("Size: ", members.size);
 };
 
 let scrapeMembers = () => {
-  let memberElems = getMemberElems();
-  addElemsTextToMembers(memberElems);
-  scrollDown();
-  let currentMemberSize = members.size;
+    let memberElems = getMemberElems();
+    addElemsTextToMembers(memberElems);
+    scrollDown();
+    let currentMemberSize = members.size;
 
-  updateConsecutiveSameSizes(currentMemberSize);
+    updateConsecutiveSameSizes(currentMemberSize);
 
-  let shouldStop = consecutiveSameSizes > MAX_CONSECUTIVE_SAME_SIZES;
-  if (shouldStop) {
-    stopScraping();
-    giveOutput();
-  }
+    let shouldStop = consecutiveSameSizes > MAX_CONSECUTIVE_SAME_SIZES;
+    if (shouldStop) {
+        stopScraping();
+        giveOutput();
+    }
 };
 
 scrapingIntervalID = setInterval(scrapeMembers, 1000);
